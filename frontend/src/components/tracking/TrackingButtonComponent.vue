749846d8653e9f2c8fb7d64b50b75b53c375b1e1
Stefan Simko <stefan.simko@smilingwords.eu>
2020-12-24 23:58:01 +0100

    Added tracking component + button

---
+++ frontend/src/components/tracking/TrackingButtonComponent.vue
@@ -0,0 +1,21 @@
+<template lang="">
+  <div class="trackingButton">
+    <h2>This is tracking button component</h2>
+    <button
+      class="inline-block px-6 py-2 text-xs font-medium leading-6 text-center text-white uppercase transition bg-green-500 rounded shadow ripple hover:shadow-lg hover:bg-green-600 focus:outline-none"
+      v-on:click="counter += 1"
+    >
+      {{ counter }}
+    </button>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      counter: 0
+    };
+  }
+};
+</script>
+<style lang=""></style>
---
+++ frontend/src/views/tracking/Tracking.vue
@@ -0,0 +1,15 @@
+<template lang="">
+  <div class="tracking">
+    <TrackingButtonComponent />
+  </div>
+</template>
+<script>
+import TrackingButtonComponent from "@/components/tracking/TrackingButtonComponent.vue";
+
+export default {
+  components: {
+    TrackingButtonComponent
+  }
+};
+</script>
+<style lang=""></style>
