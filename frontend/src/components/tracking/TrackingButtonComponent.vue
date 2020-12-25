20237fb754c5575e9c58d1178ab5306fee40fa67
Stefan Simko <stefan.simko@smilingwords.eu>
2020-12-25 18:14:37 +0100

    Added counter + css for it

--- frontend/src/components/tracking/TrackingButtonComponent.vue
+++ frontend/src/components/tracking/TrackingButtonComponent.vue
@@ -1,10 +1,7 @@
 <template lang="">
-  <div class="trackingButton">
+  <div class="trackingButtonComponent">
     <h2>This is tracking button component</h2>
-    <button
-      class="inline-block px-6 py-2 text-xs font-medium leading-6 text-center text-white uppercase transition bg-green-500 rounded shadow ripple hover:shadow-lg hover:bg-green-600 focus:outline-none"
-      v-on:click="counter += 1"
-    >
+    <button class="glow-on-hover" @click="wasClicked">
       {{ counter }}
     </button>
   </div>
@@ -13,9 +10,70 @@
 export default {
   data() {
     return {
-      counter: 0
+      counter: 0,
+      clicked: false
     };
+  },
+  methods: {
+    wasClicked() {
+      this.counter += 1;
+    }
   }
 };
 </script>
-<style lang=""></style>
+<style>
+.glow-on-hover {
+  width: 220px;
+  height: 50px;
+  border: 20px;
+  outline: none;
+  color: #b83d6e;
+  background: #33ffbd;
+  cursor: pointer;
+  position: relative;
+  z-index: 0;
+  font-size: 24px;
+  border-radius: 10px;
+}
+
+.glow-on-hover:before {
+  content: "";
+  background: #33ffbd;
+  position: absolute;
+  top: -2px;
+  left: -2px;
+  background-size: 400%;
+  z-index: -1;
+  filter: blur(5px);
+  width: calc(100% + 4px);
+  height: calc(100% + 4px);
+  opacity: 0;
+  font-size: 24px;
+  transition: opacity 0.3s ease-in-out;
+  border-radius: 10px;
+}
+
+.glow-on-hover:active {
+  color: white;
+}
+
+.glow-on-hover:active:after {
+  background: transparent;
+}
+
+.glow-on-hover:hover:before {
+  opacity: 1;
+}
+
+.glow-on-hover:after {
+  z-index: -1;
+  content: "";
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  background: #f1ebd5;
+  left: 0;
+  top: 0;
+  border-radius: 10px;
+}
+</style>
